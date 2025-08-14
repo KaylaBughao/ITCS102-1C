bb2ccbe8f60f6b92b623069e693ab4b5d7841bce
Kayla Bughao <tian.macatangay@gmail.com>
2025-08-14 10:56:20 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,18 @@
+TikTok= eval(input("enter account to sitting --> "))
+print ("this is the breakdown of the dance :")
+print("1000 : ", TikTok // 1000 )
+TikTok %= 1000
+print("500 : ", TikTok // 500 )
+TikTok %= 500
+print("200 : ", TikTok // 200 )
+TikTok %= 200
+print("100 : ", TikTok // 100 )
+TikTok %= 100
+print("50 : ", TikTok // 50 )
+TikTok %= 50
+print("20 : ", TikTok // 20 )
+TikTok %= 20
+print("5 : ", TikTok // 5 )
+TikTok %= 5
+print("1 : ", TikTok // 1 )
+TikTok %= 1
